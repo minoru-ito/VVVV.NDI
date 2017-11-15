0e47888ceda5fc1037735239678a695c8a699ac0
mino <minoru0218@gamil.com>
2017-11-15 09:36:17 +0900

    1st commit

---
+++ VVVV.NDI/TesterNode.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using VVVV.Core.Logging;
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using NewTek;
+using NewTek.NDI;
+
+namespace VVVV.DX11.Nodes
+{
+    namespace VVVV.NDI
+    {
+        [PluginInfo(Name = "Tester", Category = "NDI")]
+        public class TesterNode : IPluginEvaluate, IPartImportsSatisfiedNotification, IDisposable
+        {
+            [Import()]
+            ILogger FLogger;
+
+            bool initialized = false;
+            bool disposed = false;
+
+            public void OnImportsSatisfied()
+            {
+                // Not required, but "correct". (see the SDK documentation)
+                if (!NDIlib.initialize())
+                {
+                    // Cannot run NDI. Most likely because the CPU is not sufficient (see SDK documentation).
+                    // you can check this directly with a call to NDIlib_is_supported_CPU()
+                    FLogger.Log(LogType.Error, "Cannot run NDI");
+                }
+                else
+                {
+                    FLogger.Log(LogType.Message, NDIlib.version().ToString());
+
+                    initialized = true;
+                }
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+            }
+
+            private void Dispose(bool disposing)
+            {
+                if(disposing)
+                {
+                    if(!disposed)
+                    {
+                        disposed = true;
+
+                        // Not required, but "correct". (see the SDK documentation)
+                        NDIlib.destroy();
+                    }
+                }
+            }
+
+            public void Evaluate(int SpreadMax)
+            {
+
+            }
+        }
+    }
+}
